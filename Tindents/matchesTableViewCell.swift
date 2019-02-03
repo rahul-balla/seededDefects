b6eac792b5020d04086ececc89b12a157b85b28e
andre <chang432@purdue.edu>
2019-02-03 18:58:13 -0500

    stuff

---
+++ Tindents/matchesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  matchesTableViewCell.swift
+//  Tindents
+//
+//  Created by Andre Chang on 2/3/19.
+//  Copyright © 2019 Rahul Balla. All rights reserved.
+//
+
+import UIKit
+
+class matchesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tindents/profileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  profileViewController.swift
+//  Tindents
+//
+//  Created by Andre Chang on 2/3/19.
+//  Copyright © 2019 Rahul Balla. All rights reserved.
+//
+
+import UIKit
+
+class profileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
