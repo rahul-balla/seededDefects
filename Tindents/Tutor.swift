f7cff774eb5201215e842935a173004def070bcb
Rahul Balla <rballa@purdue.edu>
2019-02-06 19:45:42 -0500

    created tutor.swift

---
+++ Tindents/Tutor.swift
@@ -0,0 +1,23 @@
+//
+//  Tutor.swift
+//  Tindents
+//
+//  Created by Rahul Balla on 2/6/19.
+//  Copyright © 2019 Rahul Balla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Tutor {
+    
+    var name: String!
+    var subjects: String!
+    var tutorEmail: String!
+    var rating: String!
+    var picture: UIImage? = nil
+    
+    init (dictionary: [[String: Any]]) {
+        
+    }
+}
