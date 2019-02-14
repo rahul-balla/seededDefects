b4edfc2ec15a40ff45e0e5ab95c78166097181ec
Rahul Balla <rballa@purdue.edu>
2019-02-14 17:04:57 -0500

    added swiping feature

--- Tindents/Tutor.swift
+++ Tindents/Tutor.swift
@@ -11,19 +11,19 @@ import UIKit
 
 class Tutor {
     
-    var name: String?
-    var subjects: [String]?
-    var tutorEmail: String?
+    var name: String!
+    var subjects: [String]!
+    var tutorEmail: String!
     var rating: String?
     var picture: UIImage?
     var description: String?
     
     init (dictionary: [String: Any]) {
         name = dictionary["name"] as? String ?? "Creepy Tutor"
-        subjects = dictionary["subjects"] as? [String] ?? nil
+        subjects = dictionary["subjects"] as? [String] ?? []
         tutorEmail = dictionary["tutorEmail"] as? String ?? "No email"
         rating = dictionary["rating"] as? String ?? "No Rating"
-        picture = dictionary["picture"] as? UIImage?? nil
+        picture = dictionary["picture"] as? UIImage ?? nil
         description = dictionary["description"] as? String ?? "No Description"
     }
 }
