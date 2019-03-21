68cae6e34e853b59e714607cd79d295f7bdb69a7
Rahul Balla <rballa@purdue.edu>
2019-03-21 18:22:45 -0400

    information directly transfered from matchesTableCell to DetailViewController

--- Tindents/matchesTableViewController.swift
+++ Tindents/matchesTableViewController.swift
@@ -9,10 +9,54 @@
 import UIKit
 
 class matchesTableViewController: UITableViewController {
+    
+    var firstName : [String: Any] = [
+        "name" : "Ryan Gosling",
+        "age" : 23,
+        "subjects" : ["CS", "MATH"],
+        "tutorEmail" : "ryan@gosling.com",
+        "rating" : "4.5/5",
+        "description" : "hello",
+        "picture" : UIImage(named: "ryan")!,
+        "charge" : 23.5
+    ]
+    
+    var secondName : [String: Any] = [
+        "name" : "Tom Cruise",
+        "age" : 60,
+        "subjects" : ["PHIL", "CS"],
+        "tutorEmail" : "tom@cruise.com",
+        "rating" : "3.8/5",
+        "description" : "my name is Tom Cruise",
+        "picture" : UIImage(named: "tomCruise")!,
+        "charge" : 30.98
+    ]
+    
+    var thirdName : [String: Any] = [
+        "name" : "Will Smith",
+        "age" : 89,
+        "subjects" : ["STAT", "CS"],
+        "tutorEmail" : "will@smith.com",
+        "rating" : "2.1/5",
+        "description" : "CS 354 (OS) sucks!",
+        "picture" : UIImage(named: "willSmith")!,
+        "charge" : 45.98
+    ]
+    
+    var tutors: [Tutor] = []
 
+    @IBOutlet var matchesTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let tutor1 = Tutor(dictionary: firstName)
+        let tutor2 = Tutor(dictionary: secondName)
+        let tutor3 = Tutor(dictionary: thirdName)
+        
+        self.tutors.append(tutor1)
+        self.tutors.append(tutor2)
+        self.tutors.append(tutor3)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -29,17 +73,38 @@ class matchesTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return tutors.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "matchCell", for: indexPath) as! matchesTableViewCell
 
+        let tutor = tutors[indexPath.row]
+        cell.tutor = tutor
+        
         return cell
     }
  
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell){
+            let tutor = tutors[indexPath.row]
+            let detailViewController = segue.destination as! profileViewController
+            detailViewController.tutor = tutor
+        }
+    }
 
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let cell = sender as! UITableViewCell
+//        if let indexPath = tableView.indexPath(for: cell){
+//            let movie = movies[indexPath.row];
+//            let detailViewController = segue.destination as! DetailViewController
+//            detailViewController.movie = movie;
+//        }
+//
+//    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
