803ba53d2bc784cdd71feef60bf1cb49a4d84c32
harshav16 <vallurip@purdue.edu>
2019-03-21 20:32:07 -0400

    filter defect

--- Backend/files/__init__.py
+++ Backend/files/__init__.py
@@ -10,6 +10,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
+import json
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
@@ -44,6 +45,22 @@ class users(UserMixin, db.Model):
     password = db.Column(db.String(50), nullable=False)
     account_type = db.Column(db.String(10), nullable = False)
     fullName = db.Column(db.String(50))
+    settings = db.Column(db.String(1000))
+    onCampus = db.Column(db.Integer)
+    offCampus = db.Column(db.Integer)
+    fs = db.Column(db.Integer)
+    js = db.Column(db.Integer)
+    cheap = db.Column(db.Integer)
+    medium = db.Column(db.Integer)
+    expensive = db.Column(db.Integer)
+    com = db.Column(db.Integer)
+    cs = db.Column(db.Integer)
+    bio = db.Column(db.Integer)
+    econ = db.Column(db.Integer)
+    chem = db.Column(db.Integer)
+    english = db.Column(db.Integer)
+    physics = db.Column(db.Integer)
+    math = db.Column(db.Integer)
 
     def __repr__(self):
         return "users('{self.username}', {self.email}', {self.password}', {self.fullName}')"
@@ -68,9 +85,6 @@ def createAccount():
     	user = users(username = content["username"], password = content["password"], email = content["email"], account_type = content["account_type"], fullName = content["name"])
     	db.session.add(user)
     	db.session.commit()
-    
-    
-    	print("accout has been added to database")
 
     	return jsonify({'success' : 1})
 
@@ -105,35 +119,62 @@ def profile():
 @app.route("/settings", methods=['GET', 'POST'])
 def settings():
     content = request.json
+    print(content)
 
-    db.engine.execute("UPDATE users SET settings = %s WHERE id = %s", str(content), userid)
+
+    db.engine.execute("UPDATE users SET settings = %s, onCampus = %s, offCampus = %s, fs = %s, js = %s, cheap = %s, medium = %s, expensive = %s, com = %s, cs = %s, bio = %s, econ = %s, chem = %s, english = %s, physics = %s, math = %s WHERE id = %s", str(content), content["oncSwitchOn"], content["offcSwitchOn"],content["fsSwitchOn"], content["jsSwitchOn"], content["cheapSwitchOn"], content["medSwitchOn"], content["expensiveSwitchOn"], content["comSwitchOn"], content["csSwitchOn"], content["bioSwitchOn"], content["econSwitchOn"],content["chemSwitchOn"], content["englishSwitchOn"], content["physicsSwitchOn"], content["mathSwitchOn"], userid)
 
     return jsonify({'success' : 1})
     
 
 @app.route("/feed", methods=['GET', 'POST'])
 def feed():
-    
     userDict = []
 
+    print("jfneononjovjnotnojn")
+    
     if accountType == "student" :
+        currUser = users.query.filter_by(id = userid).first()
+
         feed = users.query.filter_by(account_type = "tutor")
+
         for x in feed:
-            matchCheck = db.engine.execute("SELECT COUNT(id) FROM matches WHERE student_id = %s and tutor_id = %s", userid,x.id).scalar()
+            matchCheck = db.engine.execute("SELECT COUNT(id) FROM matches WHERE student_id = %s and tutor_id = %s and (student_swipe = 1 or student_swipe = 2)", userid,x.id).scalar()
             #             matchCheck = matches.query.filter_by(student_id = userid, tutor_id = x.id).count()
 
 
             if matchCheck == 0:
-                dd = {'username' : x.username, 'userid' : x.id, 'email':x.email, 'fullName':x.fullName}
-                userDict.append(dd)
+                if currUser.settings == None:
+                    dd = {'username' : x.username, 'userid' : x.id, 'email':x.email, 'fullName':x.fullName}
+                    userDict.append(dd)
+                    print("In the first if statement\n")
+
+                
+                if (currUser.onCampus == 1 and (x.onCampus == 0 or x.onCampus == None)) or (currUser.offCampus == 1 and (x.offCampus == 0 or x.offCampus == None)) or (currUser.fs == 1 and (x.fs == 0 or x.fs == None)) or (currUser.js == 1 and (x.js == 0 or x.js == None)) or (currUser.cheap == 1 and (x.cheap == 0 or x.cheap == None)) or (currUser.medium == 1 and (x.medium == 0 or x.medium == None)) or (currUser.expensive == 1 and (x.expensive == 0 or x.expensive == None)) or (currUser.com == 1 and (x.com == 0 or x.com == None)) or (currUser.cs == 1 and (x.cs == 0 or x.cs == None)) or (currUser.bio == 1 and (x.bio == 0 or x.bio == None))  or (currUser.econ == 1 and (x.econ == 0 or x.econ == None)) or (currUser.chem == 1 and (x.chem == 0 or x.chem == None)) or (currUser.english == 1 and (x.english == 0 or x.english == None)) or (currUser.physics == 1 and (x.physics == 0 or x.physics == None)) or (currUser.math == 1 and (x.math == 0 or x.math == None)) :
+                    print("In the second if statement\n")
+                else:
+                    print("in the else statement")
+                    dd = {'username' : x.username, 'userid' : x.id, 'email':x.email, 'fullName':x.fullName}
+                    userDict.append(dd)
+
     else:
+        currUser = users.query.filter_by(id = userid).first()
+
         feed = users.query.filter_by(account_type = "student")
-        for x in feed:
-            matchCheck = db.engine.execute("SELECT COUNT(id) FROM matches WHERE student_id = %s and tutor_id = %s", x.id, userid).scalar()
-            if matchCheck == 0:
-                dd = {'username' : x.username, 'userid' : x.id, 'email':x.email, 'fullName':x.fullName}
-                userDict.append(dd)
         
+        for x in feed:
+            matchCheck = db.engine.execute("SELECT COUNT(id) FROM matches WHERE student_id = %s and tutor_id = %s and (tutor_swipe = 1 or tutor_swipe = 2)", x.id, userid).scalar()
+            if matchCheck == 0:
+                if currUser.settings == None:
+                    dd = {'username' : x.username, 'userid' : x.id, 'email':x.email, 'fullName':x.fullName}
+                    userDict.append(dd)
+                
+                if (currUser.onCampus == 1 and (x.onCampus == 0 or x.onCampus == None)) or (currUser.offCampus == 1 and (x.offCampus == 0 or x.offCampus == None)) or (currUser.fs == 1 and (x.fs == 0 or x.fs == None)) or (currUser.js == 1 and (x.js == 0 or x.js == None)) or (currUser.cheap == 1 and (x.cheap == 0 or x.cheap == None)) or (currUser.medium == 1 and (x.medium == 0 or x.medium == None)) or (currUser.expensive == 1 and (x.expensive == 0 or x.expensive == None)) or (currUser.com == 1 and (x.com == 0 or x.com == None)) or (currUser.cs == 1 and (x.cs == 0 or x.cs == None)) or (currUser.bio == 1 and (x.bio == 0 or x.bio == None))  or (currUser.econ == 1 and (x.econ == 0 or x.econ == None)) or (currUser.chem == 1 and (x.chem == 0 or x.chem == None)) or (currUser.english == 1 and (x.english == 0 or x.english == None)) or (currUser.physics == 1 and (x.physics == 0 or x.physics == None)) or (currUser.math == 1 and (x.math == 0 or x.math == None)) :
+                    print("In the if statement\n")
+                else:
+                    dd = {'username' : x.username, 'userid' : x.id, 'email':x.email, 'fullName':x.fullName}
+                    userDict.append(dd)
+
     print("userDict : ",userDict)
 
     return jsonify({'feed': userDict})
@@ -234,6 +275,15 @@ def matches():
 
     return jsonify({'matches' : matchData})
     
+
+
+@app.route("/schedule", methods=['GET', 'POST'])
+def schedule():
+    content = request.json
+
+    db.engine.execute("UPDATE users SET schedule = %s WHERE id = %s", str(content), userid)
+
+    return jsonify({'success' : 1})
 
 
 
